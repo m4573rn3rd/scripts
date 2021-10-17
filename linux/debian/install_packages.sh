6f1fb3ada40cd7279f3489dd89e0cf421b8e47f8
Brandon <brandonmcclain@live.com>
2021-10-17 12:43:29 -0500

    octv1

--- linux/debian/install_packages.sh
+++ linux/debian/install_packages.sh
@@ -1,2 +1,32 @@
 # Script to install packages for a list.
 # Because why not
+
+#!/bin/bash
+
+set -eu -o pipefail # If there is an error fail and report it also debug all lines 
+
+sudo -n true
+test $? -eq 0 || exit 1 "You have to have sudo please run sudo touch 1 then try again"
+
+echo "installing the must-have pre-requisites"
+while read -r p ; do sudo apt-get install -y $p ; done < <(cat << "EOF"
+    perl
+    zip 
+    unzip
+    gimp
+    vim
+    vlc
+    tmux
+    espeak
+    curl
+    wget
+    
+EOF
+)
+
+echo installing the nice-to-have pre-requisites
+echo you have 5 seconds to proceed ...
+echo or
+echo hit Ctrl+C to quit
+echo -e "\n"
+sleep 6
