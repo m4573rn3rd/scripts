f9a6d3fba1b273c7a9bc7f483d2b9ed0ccc4b15a
brandon <brandonmcclain@live.com>
2021-11-15 08:57:33 -0600

    nov152021

--- slack/python/config.py
+++ slack/python/config.py
@@ -1,7 +1,19 @@
 import os
+from flask import Flask
+from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash
+
+app2 = Flask(__name__)
+hash = generate_password_hash('foobar')
+hash
+app2.secret_key = {hash}
+app2.config["MONGO_URI"] = "mongodb://localhost:27017/python"
+mongo = PyMongo(app2)
+
+
 # environment variables
-os.environ["SLACK_APP_TOKEN"] = "xapp-1-A02JWS7BE7R-2681824474785-1f5e8ff443f37c9c151de7d8ff32c067453d26d8ff62d3a46c215e79721fd15c"
-os.environ["SLACK_BOT_TOKEN"] = "xoxb-2650828670406-2670419769553-qxTzP6Sbh9tlqfYIA52wh1bZ"
+os.environ["SLACK_APP_TOKEN"] = ""
+os.environ["SLACK_BOT_TOKEN"] = ""
 # To Verify above code
 os.environ.get("SLACK_APP_TOKEN")
-os.environ.get("SLACK_BOT_TOKEN")    
+os.environ.get("SLACK_BOT_TOKEN") 
