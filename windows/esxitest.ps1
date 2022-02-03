a8c13688a6d8e50b46bc7c752b371cdef98714fd
Brandon <brandonmcclain@live.com>
2022-02-03 17:31:56 -0600

    20220302

---
+++ windows/esxitest.ps1
@@ -0,0 +1,5 @@
+﻿#Connect-VIServer -Server 192.168.1.100 -User bmcclain@vsphere.local -Password 
+#Connect-ViServer -Server "192.168.1.100"
+#Get-VM
+#(Get-VM).Guest.IPAddress
+Get-VMHostNetworkAdapter | select VMhost, Name, IP, SubnetMask, Mac, PortGroupName, vMotionEnabled, mtu, FullDuplex, BitRatePerSec | Export-Csv C:\Users\blm04\git\scripts\windowsVMHostNetworkDetails.csv 
